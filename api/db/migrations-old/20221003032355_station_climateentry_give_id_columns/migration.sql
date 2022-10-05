2ef07865eaa22afe3a02db3b4ea45dbb75b54882
Davis Smith <dsmith@agilmind.com>
2022-10-05 10:45:21 -0500

    removed old migrations and old schema.prism copy

--- api/db/migrations-old/20221003032355_station_climateentry_give_id_columns/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "ClimateDataPoint" ADD COLUMN "id" TEXT;
-
--- AlterTable
-ALTER TABLE "ClimateEntry" ADD COLUMN "stationId" TEXT;
-
--- AlterTable
-ALTER TABLE "Station" ADD COLUMN "id" TEXT;
